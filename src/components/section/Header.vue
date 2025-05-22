326f13b18f30c33e09577d5a0abcc0c06b04eb18
kimjunyo <junn4580@gmail.com>
2025-05-22 12:55:38 +0900

    feat: pinia install

--- src/components/section/Header.vue
+++ src/components/section/Header.vue
@@ -1,49 +1,43 @@
 <template>
   <div class="header d-flex justify-content-between align-items-center p-3">
-        <div class="logo-box">
-          <a href="/"><img class="logo" src="../../assets/images/logo.png" alt="Logo" /></a>
-        </div>
-        <div class="header-right d-flex align-items-center gap-3">
-          <div>
-            <a v-if="!isLoggedIn" @click="login">로그인</a>
-            <a v-else @click="logout">로그아웃</a>
-          </div>
-          <div class="user">
-            <img src="../../assets/images/user.png" alt="User" />
-          </div>
-        </div>
+    <div class="logo-box">
+      <a href="/"><img class="logo" src="../../assets/images/logo.png" alt="Logo" /></a>
+    </div>
+    <div class="header-right d-flex align-items-center gap-3">
+      <div>
+        <a v-if="!isLoggedIn" @click="login">로그인</a>
+        <a v-else @click="logout">로그아웃</a>
       </div>
+      <div class="user">
+        <img src="../../assets/images/user.png" alt="User" />
+      </div>
+    </div>
+  </div>
 </template>
+
 <script setup>
 import '@/assets/main.css';
 import '@/assets/base.css';
 
-import {useRouter} from 'vue-router'
+import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
 
 const router = useRouter()
+const isLoggedIn = ref(false)
+
 const login = () => {
   router.push('/user/login')
 }
-const logout = () => {
-  sessionStorage.clear();
-}
-</script>
-<script>
-export default {
-  data() {
-    return {
-      contextRoot: '', // ex) '/your-app', 이 값은 필요 시 props나 환경변수로 주입 가능
-      isLoggedIn: false // 실제 로그인 여부에 따라 설정
-    };
-  },
-  mounted() {
-    // 예시: 로그인 상태를 로컬 저장소나 API에서 확인
-    const userId = sessionStorage.getItem('id');
-    this.isLoggedIn = !!userId;
 
-    // 예시: contextRoot를 자동 설정하거나 props로 받을 수도 있음
-    this.contextRoot = import.meta.env.VITE_CONTEXT_ROOT || ''; // Vite 사용 시
-  }
-};
-  
+const logout = () => {
+  sessionStorage.clear()
+  isLoggedIn.value = false
+}
+
+onMounted(() => {
+  // 마운트될 때 로그인 여부 체크
+  const userId = sessionStorage.getItem('id')
+  isLoggedIn.value = !!userId
+  console.log(userId);
+})
 </script>
--- src/components/user/Signup.vue
+++ src/components/user/Signup.vue
@@ -3,41 +3,55 @@
     <div class="title">회원가입</div>
 
     <form action="${contextRoot}/user" method="post">
-      <input type="hidden" name="action" value="signup"/>
+      <input type="hidden" name="action" value="signup" />
       <div class="input-box">
         <div>
-          <label for="id">아이디</label><input id="id" name="id" placeholder="아이디를 입력해주세요"/>
+          <label for="id">아이디</label>
+          <input id="id" name="id" placeholder="아이디를 입력해주세요" />
         </div>
         <div>
-          <label for="name">이름</label><input id="name" name="name" placeholder="이름을 입력해주세요"/>
+          <label for="name">이름</label><input id="name" name="name" placeholder="이름을 입력해주세요" />
         </div>
         <div>
-          <label for="email">이메일</label><input type="email" id="email" name="email" placeholder="이메일을 입력해주세요"/>
+          <label for="email">이메일</label><input type="email" id="email" name="email" placeholder="이메일을 입력해주세요" />
         </div>
         <div>
           <label for="password">비밀번호</label><input type="password" id="password" name="password"
-                                                   placeholder="비밀번호를 입력해주세요"/>
+            placeholder="비밀번호를 입력해주세요" />
         </div>
         <div>
-          <label for="pscheck">비밀번호 확인</label><input type="password" id="pscheck" placeholder="비밀번호를 입력해주세요"/>
+          <label for="pscheck">비밀번호 확인</label><input type="password" id="pscheck" placeholder="비밀번호를 입력해주세요" />
         </div>
       </div>
-      <hr/>
+      <hr />
       <div class="button-box">
         <button class="signup">가입</button>
         <div class="explain-text"> 이미 아이디가 있으시다면?</div>
       </div>
     </form>
-    <button class="signin">
-      <a href="${contextRoot}/user?action=login_form">로그인</a>
+    <button class="signin" @click="requestSignin">로그인
     </button>
   </div>
 </template>
 
 <script setup>
+import { signup } from '@/api/user'
+import { ref } from 'vue'
+import router from '../router';
+const id = ref('');
+const name = ref('');
+const password = ref('');
+const pscheck = ref('');
 
+const requestSignup = async () => {
+  await signup({
+
+  })
+}
+
+const requestSignin = () => {
+  router.push('/user/login');
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
